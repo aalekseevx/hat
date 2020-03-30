af0e9b702d1a929aba3a70e752a587df4fd4c7b7
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-03-30 14:18:40 +0300

    Dictionary_full

--- backend/dictionary.py
+++ backend/dictionary.py
@@ -5,7 +5,7 @@ import string
 class Dictionary:
     def __init__(self, filename):
         self.dictionary = []
-        with open(filename) as f:
+        with open(filename, encoding='utf-8') as f:
             while True:
                 line = f.readline()
                 if not line: break
@@ -15,15 +15,24 @@ class Dictionary:
         self.dictionary = dict(self.dictionary)
 
     def GetDictForGame(self, numb, complex, variation):
+        dict_copy = self.dictionary.copy()
         inf = max(1, complex - variation)
         sup = min(100, complex + variation)
         good_dict = {}
-        for ke, value in self.dictionary.items():
-            if int(value) >= inf:
-                if int(value) <= sup:
-                    good_dict.update({ke: value})
-                else:
-                    break
+        iter_dict = {}
+        while len(good_dict) < numb:
+            for ki in list(iter_dict.keys()):
+                del dict_copy[ki]
+            iter_dict = {}
+            for ke, value in dict_copy.items():
+                if int(value) >= inf:
+                    if int(value) <= sup:
+                        iter_dict.update({ke: value})
+                    else:
+                        break
+            good_dict.update(iter_dict)
+            inf = max(inf - 1, 1)
+            sup = min(100, sup + 1)
         ret_dict = {}
         for i in range(numb):
             key = random.choice(list(good_dict.keys()))
