0c5e95dd10cc5bce04d222c73024f3f4f0075b9a
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-03-31 21:36:41 +0300

    Singleton ru, new fitches

--- backend/dictionary.py
+++ backend/dictionary.py
@@ -1,20 +1,19 @@
 import random
+import string
 
 
-class Dictionary:
+class DictionaryEng():
     def __init__(self, filename):
         self.dictionary = []
         with open(filename, encoding='utf-8') as f:
             while True:
                 line = f.readline()
-                if not line:
-                    break
+                if not line: break
                 a = line.split()
                 b = [a]
                 self.dictionary = self.dictionary + b
         self.dictionary = dict(self.dictionary)
 
-<<<<<<< HEAD
     def GetDictForGame(self, numb, complex, variation):
         dict_copy = self.dictionary.copy()
         inf = max(1, complex - variation)
@@ -29,26 +28,43 @@ class Dictionary:
                 if int(value) >= inf:
                     if int(value) <= sup:
                         iter_dict.update({ke: value})
-                    else:
-                        break
             good_dict.update(iter_dict)
             inf = max(inf - 1, 1)
             sup = min(100, sup + 1)
-=======
-    def get_dict_for_game(self, numb, complexity, variation):
-        inf = max(1, complexity - variation)
-        sup = min(100, complexity + variation)
-        good_dict = {}
-        for key, value in self.dictionary.items():
-            if int(value) >= inf:
-                if int(value) <= sup:
-                    good_dict.update({key: value})
-                else:
-                    break
->>>>>>> 94d776f782d439cc628b3e1e2e74d47b3a30a804
         ret_dict = {}
         for i in range(numb):
             key = random.choice(list(good_dict.keys()))
             val = good_dict.pop(key)
             ret_dict.update({key: val})
         return ret_dict
+
+    def AddWord(self, word, complexity):
+        complex = min(complexity, 100)
+        complex = max(1, complex)
+        self.dictionary.update({word: complex})
+
+    def DelWord(self, word):
+        if word in self.dictionary:
+            self.dictionary.pop(word)
+
+    def __len__(self):
+        return len(self.dictionary)
+
+
+def Singleton(cls):
+    def gg(str):
+        instance = {}
+
+        def wrap(str):
+            if cls not in instance:
+                instance[cls] = cls(str)
+            return instance[cls]
+
+        return wrap
+
+    return gg
+
+
+@Singleton(DictionaryEng)
+def DictionaryRu(str):
+    return DictionaryEng(str)
