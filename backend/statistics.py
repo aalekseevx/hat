067ecb3dd03161050a34df29f12d6f06f10953bd
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-04-03 17:13:56 +0300

    annotation except main

--- backend/statistics.py
+++ backend/statistics.py
@@ -1,23 +1,26 @@
 from namedlist import namedlist
+from typing import Dict, Any
 
 UserInfo = namedlist('UserInfo', ['username', ('guesses', 0), ('explanations', 0), ('mistakes', 0), ('points', 0)])
 WordInfo = namedlist('WordInfo', ['word', ('screen_time', 0), ('tries', 0), ('mistake', False)])
 
 
 class PartyStatistics:
-
-    def add_objects(self, users, words):
+    def add_objects(self, users: list, words: list) -> None:
+        """add users and words"""
         self.user_statistics.update({user: UserInfo(user, 0, 0, 0, 0) for user in users if user not in self.inside})
         self.inside.update(users)
         self.words_statistics.update({word: WordInfo(word=word) for word in words})
 
-    def __init__(self, users, words):
+    def __init__(self, users: list, words: list) -> None:
+        """PartyStatistics initialization"""
         self.inside = set()
         self.user_statistics = {}
         self.words_statistics = {}
         self.add_objects(users, words)
 
-    def add_result(self, explaining_user, guessing_user, word, verdict, screen_time):
+    def add_result(self, explaining_user: str, guessing_user: str, word: str, verdict: str, screen_time: float) -> None:
+        """update statistics"""
         if verdict == 'correct':
             self.user_statistics[explaining_user].explanations += 1
             self.user_statistics[guessing_user].guesses += 1
@@ -30,7 +33,8 @@ class PartyStatistics:
         self.words_statistics[word].screen_time += screen_time
         self.words_statistics[word].tries += 1
 
-    def get(self):
+    def get(self) -> Dict[str, Any]:
+        """return all available statistics"""
         return {
             'users': tuple(sorted([dict(user_info._asdict()) for user_info in self.user_statistics.values()],
                                   key=lambda x: x['points'], reverse=True))
