ee7404d938a42926f8ce602f621e900769651e1f
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-04-04 12:31:51 +0300

    fixed singleton

--- backend/singleton.py
+++ backend/singleton.py
@@ -3,9 +3,9 @@ def singleton(cls):
         instance = {}
 
         def wrap(*args, **kwargs):
-            if (cls, *args, tuple(**kwargs)) not in instance.keys():
-                instance[(cls, *args, tuple(**kwargs))] = cls(*args, **kwargs)
-            return instance[(cls, *args, tuple(**kwargs))]
+            if (cls, *args, frozenset(kwargs.items())) not in instance.keys():
+                instance[(cls, *args, frozenset(kwargs.items()))] = cls(*args, **kwargs)
+            return instance[(cls, *args, frozenset(kwargs.items()))]
 
         return wrap
 
