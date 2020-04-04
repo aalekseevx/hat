ca3455553a09fcc66015b8d8f0e8499f70794fdd
Alekseev Aleksandr <alekseev2002@inbox.ru>
2020-04-05 01:48:30 +0300

    Added a fiction test

--- backend/tests/dictionary_test.py
+++ backend/tests/dictionary_test.py
@@ -1,6 +1,10 @@
 # from dictionary import Dictionary
-#
-#
+
+
+def test_simple():
+    assert 1 == 1
+
+
 # def test_is_singleton():
 #     obj1 = Dictionary("dictionary_sample.txt")
 #     obj2 = Dictionary("dictionary_sample.txt")
