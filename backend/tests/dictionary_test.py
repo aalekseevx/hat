a79bfe3ee8d75680a56da20660b55eaf132ac92a
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-04-26 22:31:31 +0300

    backend tests

--- backend/tests/dictionary_test.py
+++ backend/tests/dictionary_test.py
@@ -1,15 +1,6 @@
 import pytest
 
 
-@pytest.mark.usefixtures("app")
-def test_is_singleton(app):
-    with app.app_context():
-        from app.main.config_helper import get_dict_by_name
-        obj1 = get_dict_by_name("Sample")
-        obj2 = get_dict_by_name("Sample")
-    assert obj1 is obj2
-
-
 @pytest.fixture
 def create_dict(app):
     from app.main.config_helper import get_dict_by_name
